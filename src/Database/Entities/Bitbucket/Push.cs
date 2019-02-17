c4005cd5fa8444c6cca6b01b4cf3f5dbcd2e898d
Mike Griffith <mwgriffith@gmail.com>
2019-02-17 17:19:00 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Database/Entities/Bitbucket/Push.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database.Entities.Bitbucket
+{
+    /// <summary>
+    /// Entity that contains the GitHub Commit messages.
+    /// </summary>
+    public class Push : BuildDbEntity
+    {
+        public string PushMessage { get; set; }
+
+
+        /// <summary>
+        /// Static function which creates the table for this entity.
+        /// </summary>
+        /// <param name="mb">ModelBuilder used to create the entity.</param>
+        public static void CreateTable(ModelBuilder mb)
+        {
+            mb.Entity<Github.Commit>(e =>
+            {
+                e.ToTable("bitbucket.push");
+
+                e.Property(p => p.CommitMessage)
+                    .HasColumnName("pushmessage")
+                    .HasColumnType("jsonb")
+                    .IsRequired();
+            });
+        }
+    }
+}
---
+++ src/Database/Migrations/20190217220712_AddedBitbucketpush.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Database.Migrations
+{
+    public partial class AddedBitbucketpush : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "builddb");
+
+            migrationBuilder.CreateTable(
+                name: "bitbucket.push",
+                schema: "builddb",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    created_at = table.Column<DateTime>(nullable: false),
+                    modified_at = table.Column<DateTime>(nullable: false),
+                    deleted_at = table.Column<DateTime>(nullable: true),
+                    pushmessage = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_bitbucket.push", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BitbucketPushes",
+                schema: "builddb",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    created_at = table.Column<DateTime>(nullable: false),
+                    modified_at = table.Column<DateTime>(nullable: false),
+                    deleted_at = table.Column<DateTime>(nullable: true),
+                    PushMessage = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_BitbucketPushes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bitbucket.push",
+                schema: "builddb");
+
+            migrationBuilder.DropTable(
+                name: "BitbucketPushes",
+                schema: "builddb");
+        }
+    }
+}
